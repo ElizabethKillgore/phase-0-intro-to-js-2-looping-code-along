995771de4299db2259151fe9bcd75a5fb3e36154
Elizabeth <ek@elizabethkillgore.com>
2023-06-02 16:45:27 -0500

    finally right?

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+const cards = [];
+
+function writeCards(arrayOfStringNames, surprise) {
+    for (let i = 0; i < arrayOfStringNames.length; i++) {
+    console.log(`Thank you, ${arrayOfStringNames[i]}, for the wonderful surprise gift!`);
+    cards.push(`Thank you, ${arrayOfStringNames[i]}, for the wonderful surprise gift!`)
+  };
+return cards;
+}
+
+
+
+function countDown(number) {
+
+    while (number >= 0) {
+        console.log(number--)
+    }
+}
